0ae77638b34fe74e3ab0dc4ebeba1d3f61dfe97d
Repays <alex.chytiewicz@gmail.com>
2015-03-14 16:05:39 +0100

    Powinno działać już ;\

---
+++ InteraktywneMenu/app/src/main/java/asertywnyalekcompany/interaktywnemenu/FirstActivity.java
@@ -0,0 +1,53 @@
+package asertywnyalekcompany.interaktywnemenu;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class FirstActivity extends ActionBarActivity
+{
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    /**Interakcja po naciśnięciu na Button**/
+    public void SendMessage(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String wiadomosc = editText.getText().toString();
+        TextView textView = (TextView)findViewById(R.id.edit_message);
+        textView.setText(wiadomosc);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
